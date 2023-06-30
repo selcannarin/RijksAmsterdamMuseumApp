46a52a528236cffc6febaca79ba8a38b7d829d03
selcannarin <narinselcan@gmail.com>
2023-07-01 00:28:23 +0300

    add adapter, set ui logic

---
+++ app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/adapter/ArtObjectAdapter.kt
@@ -0,0 +1,53 @@
+package com.selcannarin.rijksamsterdammuseumapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.selcannarin.rijksamsterdammuseumapp.R
+import com.selcannarin.rijksamsterdammuseumapp.data.model.ArtObject
+import com.selcannarin.rijksamsterdammuseumapp.databinding.ItemArtobjectBinding
+import com.selcannarin.rijksamsterdammuseumapp.util.loadUrl
+
+class ArtObjectAdapter(
+    private val artObjectList: List<ArtObject>
+) : ListAdapter<ArtObject, ArtObjectAdapter.ViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_artobject, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+        return artObjectList.size
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(artObjectList[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemArtobjectBinding.bind(itemView)
+        fun bind(artObject: ArtObject) {
+            binding.textViewArtist.text = artObject.principalOrFirstMaker
+            binding.textViewArtworkTitle.text = artObject.title
+            artObject.webImage.let { binding.imageViewArtwork.loadUrl(it.url) }
+
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<ArtObject>() {
+        override fun areItemsTheSame(oldItem: ArtObject, newItem: ArtObject): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ArtObject, newItem: ArtObject): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/ui/home/HomeFragment.kt
@@ -1,27 +1,59 @@
 package com.selcannarin.rijksamsterdammuseumapp.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.selcannarin.rijksamsterdammuseumapp.adapter.ArtObjectAdapter
+import com.selcannarin.rijksamsterdammuseumapp.data.model.ArtObject
 import com.selcannarin.rijksamsterdammuseumapp.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        updateArtObjectState()
         return binding.root
     }
 
+    private fun updateArtObjectState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            homeViewModel.getArtObject()
+            homeViewModel.artObjectState.collect {
+                //  if (it.isLoading) { }
+
+                if (it.artObjectList?.artObjects?.isNotEmpty() == true) {
+                    initRecycler(it.artObjectList.artObjects)
+                } else {
+                    Toast.makeText(context, "No Data Found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initRecycler(list: List<ArtObject>) {
+        binding.rvArtobjects.apply {
+            adapter = ArtObjectAdapter(list)
+        }
+
+    }
 }
+
