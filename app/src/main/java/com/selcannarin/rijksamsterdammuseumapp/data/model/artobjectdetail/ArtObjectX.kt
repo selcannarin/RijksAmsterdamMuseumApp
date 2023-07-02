a05fd3320cd53ee16254f3d01c6d03ad9fe46d80
selcannarin <narinselcan@gmail.com>
2023-07-02 17:37:52 +0300

    create artobjectdetail models

---
+++ app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/data/model/artobjectdetail/ArtObjectX.kt
@@ -0,0 +1,31 @@
+package com.selcannarin.rijksamsterdammuseumapp.data.model.artobjectdetail
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ArtObjectX(
+
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("dating")
+    val dating: Dating,
+    @SerializedName("longTitle")
+    val longTitle: String,
+    @SerializedName("materials")
+    val materials: List<String>,
+    @SerializedName("objectCollection")
+    val objectCollection: List<String>,
+    @SerializedName("objectNumber")
+    val objectNumber: String,
+    @SerializedName("objectTypes")
+    val objectTypes: List<String>,
+    @SerializedName("principalMaker")
+    val principalMaker: String,
+    @SerializedName("productionPlaces")
+    val productionPlaces: List<String>,
+    @SerializedName("techniques")
+    val techniques: List<Any>,
+    @SerializedName("title")
+    val title: String
+
+)
---
+++ app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/data/model/artobjectdetail/Dating.kt
@@ -0,0 +1,9 @@
+package com.selcannarin.rijksamsterdammuseumapp.data.model.artobjectdetail
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Dating(
+    @SerializedName("presentingDate")
+    val presentingDate: String
+)
