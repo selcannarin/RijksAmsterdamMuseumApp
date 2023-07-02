3713ac08b1144e941cd62bda5b5df7ac7599819a
selcannarin <narinselcan@gmail.com>
2023-07-02 18:22:35 +0300

    create artobject detail data sources and repository

--- app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/di/DataModule.kt
+++ app/src/main/java/com/selcannarin/rijksamsterdammuseumapp/di/DataModule.kt
@@ -2,7 +2,11 @@ package com.selcannarin.rijksamsterdammuseumapp.di
 
 import com.selcannarin.rijksamsterdammuseumapp.data.datasource.RemoteMuseumDataSource
 import com.selcannarin.rijksamsterdammuseumapp.data.datasource.RemoteMuseumDataSourceImpl
+import com.selcannarin.rijksamsterdammuseumapp.data.datasource.RemoteMuseumDetailDataSource
+import com.selcannarin.rijksamsterdammuseumapp.data.datasource.RemoteMuseumDetailDatasourceImpl
 import com.selcannarin.rijksamsterdammuseumapp.data.remote.MuseumApiService
+import com.selcannarin.rijksamsterdammuseumapp.data.repository.MuseumDetailRepository
+import com.selcannarin.rijksamsterdammuseumapp.data.repository.MuseumDetailRepositoryImpl
 import com.selcannarin.rijksamsterdammuseumapp.data.repository.MuseumRepository
 import com.selcannarin.rijksamsterdammuseumapp.data.repository.MuseumRepositoryImpl
 import dagger.Module
@@ -22,5 +26,15 @@ object DataModule {
     @Provides
     fun providesApiRemoteRepository(remoteMuseumDataSource: RemoteMuseumDataSource): MuseumRepository {
         return MuseumRepositoryImpl(remoteMuseumDataSource)
+    }
+
+    @Provides
+    fun providesDetailApiDataSource(museumApiService: MuseumApiService): RemoteMuseumDetailDataSource {
+        return RemoteMuseumDetailDatasourceImpl(museumApiService)
+    }
+
+    @Provides
+    fun providesDetailApiRepository(remoteMuseumDetailDataSource: RemoteMuseumDetailDataSource): MuseumDetailRepository {
+        return MuseumDetailRepositoryImpl(remoteMuseumDetailDataSource)
     }
 }
